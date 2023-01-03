752505cfe858d0d0569146eb0b61fb004c3e479a
Wajid Hussain <wajidhussain1@live.com>
2023-01-03 15:04:55 +0000

    Displaying the name and the image
    Was able to display the Pokémon name and image

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import "./App.css";
 
 function App() {
   const [pokemonName, setPokemonName] = useState("");
-  const [pokemon, setPokemon] = useState([])
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({name: "", species: "", img: "", hp: "", attack: "", defence: "", type: "",});
   const searchPokemon = () => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => { setPokemon({ name: pokemonName, species: response.data.species.name, img: response.data.sprites.front_default, hp: response.data.stats[0].base_stat, attack: response.data.stats[1].base_stat, defence: response.data.stats[2].base_stat, type: response.data.types[0].type.name,}) })
-  }
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => { setPokemon({ name: pokemonName, species: response.data.species.name, img: response.data.sprites.front_default, hp: response.data.stats[0].base_stat, attack: response.data.stats[1].base_stat, defence: response.data.stats[2].base_stat, type: response.data.types[0].type.name,}); setPokemonChosen(true) })
+  };
 
   return (
     <>
@@ -16,6 +17,13 @@ function App() {
         <h1>Pokemon Pokidex</h1>
         <input type="text" onChange={(event) => { setPokemonName(event.target.value) }} />
         <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+      <div class="displaySection">{!pokemonChosen ? (<h1>Please Choose a Pokemon</h1>) : (
+      <>
+      <h1>{pokemon.name}</h1>
+      <img src={pokemon.img} />
+      </>
+      )}
       </div>
     </>
   );
