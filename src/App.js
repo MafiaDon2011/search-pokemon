3ee040f8c0b38ca8d1c2696829d844807d48bf05
Wajid Hussain <wajidhussain1@live.com>
2023-01-03 15:28:19 +0000

    Completed Search Pokemon
    I completed the webpage that would search for stats and the image based on the name of the Pokémon.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 
@@ -10,18 +10,29 @@ function App() {
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response) => { setPokemon({ name: pokemonName, species: response.data.species.name, img: response.data.sprites.front_default, hp: response.data.stats[0].base_stat, attack: response.data.stats[1].base_stat, defence: response.data.stats[2].base_stat, type: response.data.types[0].type.name,}); setPokemonChosen(true) })
   };
 
+const checkForEnterKey = (event) => {
+  if (event.key === 'Enter') {
+    searchPokemon()
+  }
+}
+
   return (
     <>
       <div class="header">
         <div><img src="../assets/pokemon-icon.png"></img></div>
         <h1>Pokemon Pokidex</h1>
-        <input type="text" onChange={(event) => { setPokemonName(event.target.value) }} />
+        <input onKeyDown={checkForEnterKey} type="text" onChange={(event) => { setPokemonName(event.target.value) }} />
         <button onClick={searchPokemon}>Search Pokemon</button>
       </div>
       <div class="displaySection">{!pokemonChosen ? (<h1>Please Choose a Pokemon</h1>) : (
       <>
       <h1>{pokemon.name}</h1>
       <img src={pokemon.img} />
+      <h3>Species: {pokemon.species}</h3>
+      <h3>Type: {pokemon.type}</h3>
+      <h4>HP: {pokemon.hp}</h4>
+      <h4>ATTACK: {pokemon.attack}</h4>
+      <h4>DEFENCE: {pokemon.defence}</h4>
       </>
       )}
       </div>
